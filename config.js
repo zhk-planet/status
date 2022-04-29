9bd9d6a87b922ac10a42aeae685657ad622bdadf
Blhcm <54273219+blhcm@users.noreply.github.com>
2022-04-29 00:49:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm791542763-7f7e32c9f12075191f76a911',
+    'm791589155-c278c7eef1dfd1b0d0fd2060',
+    'm791589153-eb8b8b7989cfa0f7a846f9bd',  
+    'm791566932-bb08b1488c4f4dfefd5cd9cc',
   ],
 
   // 是否显示监测站点的链接
